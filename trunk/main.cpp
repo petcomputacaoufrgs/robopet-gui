5b5e18ae097bbafcb5b0be9b9ec3d87c34df73d3
silveira.kaue@gmail.com <silveira.kaue@gmail.com@a91705fc-e0f9-11de-b950-c1e2c078a2c5>
2010-01-22 18:45:54 +0000

    Commiting all robopet. Communication done. All the arrows working. Did some corrections of the makefile of the robopet-communication and created the makefile of the gui.

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   main.cpp
  * Author: pet
  *
@@ -6,12 +6,51 @@
  */
 
 #include <stdlib.h>
+#include <stdio.h>
 
-/*
- * 
- */
-int main(int argc, char** argv) {
-    1 + 4;
-    return (EXIT_SUCCESS);
+#include "ssl_client.h"
+#include "ssl_server.h"
+
+RoboCupSSLServer guitoai(PORT_GUI_TO_AI, IP_GUI_TO_AI);
+RoboCupSSLClient aitogui(PORT_AI_TO_GUI, IP_AI_TO_GUI);
+
+int DEBUG = 1;
+
+void receive()
+{
+	SSL_WrapperPacket packet;
+	if (aitogui.receive(packet) && packet.has_aitogui()) {
+		printf("----------------------------");
+		printf("Received AI-To-GUI!\n");
+	}
 }
 
+void send()
+{
+	 SSL_WrapperPacket packet;
+
+	 GUIToAI *guitoaiPacket = packet.mutable_guitoai();
+
+	 guitoaiPacket->set_nada(0);
+
+	 guitoai.send(packet);
+	printf("Sent GUI-To-AI\n");
+}
+
+/*
+ *
+ */
+int main(int argc, char** argv) {
+	printf("GUI Running!\n");
+
+	aitogui.open(false);
+
+	printf("Press <Enter> to open connection with client...\n");
+	getchar();
+	guitoai.open();
+
+	while(1) {
+		receive();
+		send();
+	}
+}
