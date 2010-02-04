428f8e3c6109467b8ab320414deb4a1eadac29af
silveira.kaue@gmail.com <silveira.kaue@gmail.com@a91705fc-e0f9-11de-b950-c1e2c078a2c5>
2010-02-04 22:08:31 +0000

    Commiting all robopet. Added radio_to_tracker proto. Beginning of gui on console. Added TODO.txt.

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -1,15 +1,14 @@
-/*
- * File:   main.cpp
- * Author: pet
- *
- * Created on 4 de Dezembro de 2009, 17:05
- */
-
 #include <stdlib.h>
 #include <stdio.h>
 
 #include "ssl_client.h"
 #include "ssl_server.h"
+
+#define GUI_WIDTH 75
+#define GUI_HEIGHT 50
+
+#define FIELD_WIDTH 6.05
+#define FIELD_HEIGHT 4.05
 
 RoboCupSSLServer guitoai(PORT_GUI_TO_AI, IP_GUI_TO_AI);
 RoboCupSSLClient aitogui(PORT_AI_TO_GUI, IP_AI_TO_GUI);
@@ -22,6 +21,8 @@ void receive()
 	if (aitogui.receive(packet) && packet.has_aitogui()) {
 		printf("----------------------------");
 		printf("Received AI-To-GUI!\n");
+		
+		
 	}
 }
 
@@ -37,13 +38,81 @@ void send()
 	printf("Sent GUI-To-AI\n");
 }
 
-/*
- *
- */
+
+
+void clrscr(void) {
+	for (int i = 0; i < 100; i++)
+		putchar('\n');
+}
+
+int gotoxy(int x, int y) {
+	char essq[100]; // String variable to hold the escape sequence
+	char xstr[100]; // Strings to hold the x and y coordinates
+	char ystr[100]; // Escape sequences must be built with characters
+
+	/*
+	** Convert the screen coordinates to strings
+	*/
+
+	sprintf(xstr, "%d", x);
+	sprintf(ystr, "%d", y);
+
+	/*
+	** Build the escape sequence (vertical move)
+	*/
+
+	essq[0] = '\0';
+	strcat(essq, "\033[");
+	strcat(essq, ystr);
+
+	/*
+	** Described in man terminfo as vpa=\E[%p1%dd
+	** Vertical position absolute
+	*/
+
+	strcat(essq, "d");
+
+	/*
+	** Horizontal move
+	** Horizontal position absolute
+	*/
+
+	strcat(essq, "\033[");
+	strcat(essq, xstr);
+
+	// Described in man terminfo as hpa=\E[%p1%dG
+	strcat(essq, "G");
+
+	/*
+	** Execute the escape sequence
+	** This will move the cursor to x, y
+	*/
+
+	printf("%s", essq);
+	return 0;
+}
+
+void process()
+{
+	
+}
+
 int main(int argc, char** argv) {
 	printf("GUI Running!\n");
+	printf("Set terminal size to %i %i\n", GUI_WIDTH, GUI_HEIGHT + 1);
 
 	aitogui.open(false);
+
+	/*
+	while(1) {
+		int i, j;
+		scanf("%i %i", &i, &j);
+		clrscr();
+		gotoxy(i, j);
+		printf("x");
+		gotoxy(0, GUI_HEIGHT + 1);
+		printf(">");
+	}//*/
 
 	printf("Press <Enter> to open connection with client...\n");
 	getchar();
@@ -51,6 +120,7 @@ int main(int argc, char** argv) {
 
 	while(1) {
 		receive();
+		process();
 		send();
 	}
 }
--- trunk/nbproject/Makefile-Release.mk
+++ trunk/nbproject/Makefile-Release.mk
@@ -31,6 +31,11 @@ OBJECTDIR=build/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
+	${OBJECTDIR}/_ext/grupos/RoboPET/robopet/robopet-gui/source/mainWindow.o \
+	${OBJECTDIR}/_ext/grupos/RoboPET/robopet/robopet-gui/source/sock.o \
+	${OBJECTDIR}/_ext/grupos/RoboPET/robopet/robopet-gui/source/server.o \
+	${OBJECTDIR}/_ext/grupos/RoboPET/robopet/robopet-gui/source/displaySettings.o \
+	${OBJECTDIR}/_ext/grupos/RoboPET/robopet/robopet-gui/source/interface.o \
 	${OBJECTDIR}/main.o
 
 # C Compiler Flags
@@ -56,6 +61,31 @@ LDLIBSOPTIONS=
 dist/Release/GNU-Linux-x86/robopet-gui: ${OBJECTFILES}
 	${MKDIR} -p dist/Release/GNU-Linux-x86
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/robopet-gui ${OBJECTFILES} ${LDLIBSOPTIONS} 
+
+${OBJECTDIR}/_ext/grupos/RoboPET/robopet/robopet-gui/source/mainWindow.o: nbproject/Makefile-${CND_CONF}.mk /grupos/RoboPET/robopet/robopet-gui/source/mainWindow.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/grupos/RoboPET/robopet/robopet-gui/source
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/grupos/RoboPET/robopet/robopet-gui/source/mainWindow.o /grupos/RoboPET/robopet/robopet-gui/source/mainWindow.cpp
+
+${OBJECTDIR}/_ext/grupos/RoboPET/robopet/robopet-gui/source/sock.o: nbproject/Makefile-${CND_CONF}.mk /grupos/RoboPET/robopet/robopet-gui/source/sock.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/grupos/RoboPET/robopet/robopet-gui/source
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/grupos/RoboPET/robopet/robopet-gui/source/sock.o /grupos/RoboPET/robopet/robopet-gui/source/sock.cpp
+
+${OBJECTDIR}/_ext/grupos/RoboPET/robopet/robopet-gui/source/server.o: nbproject/Makefile-${CND_CONF}.mk /grupos/RoboPET/robopet/robopet-gui/source/server.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/grupos/RoboPET/robopet/robopet-gui/source
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/grupos/RoboPET/robopet/robopet-gui/source/server.o /grupos/RoboPET/robopet/robopet-gui/source/server.cpp
+
+${OBJECTDIR}/_ext/grupos/RoboPET/robopet/robopet-gui/source/displaySettings.o: nbproject/Makefile-${CND_CONF}.mk /grupos/RoboPET/robopet/robopet-gui/source/displaySettings.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/grupos/RoboPET/robopet/robopet-gui/source
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/grupos/RoboPET/robopet/robopet-gui/source/displaySettings.o /grupos/RoboPET/robopet/robopet-gui/source/displaySettings.cpp
+
+${OBJECTDIR}/_ext/grupos/RoboPET/robopet/robopet-gui/source/interface.o: nbproject/Makefile-${CND_CONF}.mk /grupos/RoboPET/robopet/robopet-gui/source/interface.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/grupos/RoboPET/robopet/robopet-gui/source
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/grupos/RoboPET/robopet/robopet-gui/source/interface.o /grupos/RoboPET/robopet/robopet-gui/source/interface.cpp
 
 ${OBJECTDIR}/main.o: nbproject/Makefile-${CND_CONF}.mk main.cpp 
 	${MKDIR} -p ${OBJECTDIR}
